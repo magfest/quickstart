65ebba41fd98fc9f2d892ac20d90ecc1e907ce31
Dom <dom@magfest.org>
2015-06-12 11:10:09 -0400

    fix install

--- setup/setup-maestro.sh
+++ setup/setup-maestro.sh
@@ -8,4 +8,4 @@ wget https://bootstrap.pypa.io/get-pip.py
 python get-pip.py # install it for python 2.7
 pip2 install virtualenv
 virtualenv /home/vagrant/env
-/home/vagrant/env/bin/pip2 install --upgrade --user maestro-ng
+/home/vagrant/env/bin/pip2 install --upgrade maestro-ng
